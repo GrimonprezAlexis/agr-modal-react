5bc444ffb81793d133d2ed6a18ac63b11001bb1f
GrimonprezAlexis <70689985+alexgrz.dev@users.noreply.github.com>
2021-10-18 23:22:22 +0200

    Fix css

--- src/lib/components/modal.js
+++ src/lib/components/modal.js
@@ -11,18 +11,20 @@ require("./modal.scss");
 
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
 
+function _defineProperty(obj, key, value) { if (key in obj) { Object.defineProperty(obj, key, { value: value, enumerable: true, configurable: true, writable: true }); } else { obj[key] = value; } return obj; }
+
 var Modal = function Modal(_ref) {
   var handleClose = _ref.handleClose,
       show = _ref.show,
       title = _ref.title,
       body = _ref.body;
   var showHideClassName = show ? "modal display-block" : "modal display-none";
-  return /*#__PURE__*/_react.default.createElement("div", {
-    className: showHideClassName
-  }, /*#__PURE__*/_react.default.createElement("section", {
-    className: "modal-main"
+  return /*#__PURE__*/_react.default.createElement("div", _defineProperty({
+    className: "modal"
+  }, "className", showHideClassName), /*#__PURE__*/_react.default.createElement("section", {
+    className: "modal__main"
   }, /*#__PURE__*/_react.default.createElement("div", {
-    className: "modal-header"
+    className: "modal__header"
   }, /*#__PURE__*/_react.default.createElement("h3", null, title), /*#__PURE__*/_react.default.createElement("button", {
     type: "button",
     className: "close",
@@ -30,9 +32,9 @@ var Modal = function Modal(_ref) {
   }, /*#__PURE__*/_react.default.createElement("span", {
     "aria-hidden": "true"
   }, "\xD7"))), /*#__PURE__*/_react.default.createElement("div", {
-    className: "modal-body"
+    className: "modal__body"
   }, /*#__PURE__*/_react.default.createElement("p", null, body)), /*#__PURE__*/_react.default.createElement("div", {
-    className: "modal-footer"
+    className: "modal__footer"
   }, /*#__PURE__*/_react.default.createElement("button", {
     type: "button",
     onClick: handleClose
--- src/lib/components/modal.jsx
+++ src/lib/components/modal.jsx
@@ -5,18 +5,18 @@ export const Modal = ({ handleClose, show, title, body }) => {
   const showHideClassName = show ? "modal display-block" : "modal display-none";
 
   return (
-    <div className={showHideClassName}>
-      <section className="modal-main">
-        <div className="modal-header">
+    <div className="modal" className={showHideClassName}>
+      <section className="modal__main">
+        <div className="modal__header">
           <h3>{title}</h3>
           <button type="button" className="close" onClick={handleClose}>
             <span aria-hidden="true">×</span>
           </button>
         </div>
-        <div className="modal-body">
+        <div className="modal__body">
           <p>{body}</p>
         </div>
-        <div className="modal-footer">
+        <div className="modal__footer">
           <button type="button" onClick={handleClose}>Close</button>
         </div>
       </section>
